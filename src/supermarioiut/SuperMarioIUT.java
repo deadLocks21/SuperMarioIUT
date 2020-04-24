2b91f006516ea9243a8390caba4fb10893a84450
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-04-24 18:07:59 +0200

    Création d'un monde et affichage de ce dernier

--- src/supermarioiut/SuperMarioIUT.java
+++ src/supermarioiut/SuperMarioIUT.java
@@ -1,10 +1,13 @@
 package supermarioiut;
 
+import iut.Game;
 import iut.Vector;
 import java.awt.Color;
 import java.awt.Graphics;
 
 public class SuperMarioIUT extends iut.Game {
+    private World world = new World(14, 14);
+    private int pixelForBlock = 64;
 
     /**
      * @param args the command line arguments
@@ -18,8 +21,28 @@ public class SuperMarioIUT extends iut.Game {
         super(1024, 960, "Super Mario IUT");
     }
 
+
+    private void drawTheWorld(Game g){
+        int hauteurInverse = 1;
+        for(int y = world.getSize()[1] - 1; y >= 0; y --){
+            for(int x = 0; x < world.getSize()[0];x ++){
+                switch (world.compositionOfTheTile(x, y)){
+                    case "":
+                        break;
+                    case "floor" :
+                        Floor f = new Floor(this, x*pixelForBlock, this.getHeight() - hauteurInverse*pixelForBlock);
+                        g.addItem(f);
+                        break;
+                }
+            }
+            hauteurInverse++;
+        }
+    }
+
+
     @Override
     protected void createItems() {
+        drawTheWorld(this);
         Player j = new Player(this, (this.getWidth()/2) - 16, this.getHeight()/2  - 32);
         this.addItem(j);
     }
---
+++ src/supermarioiut/World.java
@@ -0,0 +1,98 @@
+package supermarioiut;
+
+public class World {
+    private int width;
+    private int height;
+    private String[][] compisitionOfTheWorld;
+
+    public World(int width, int height) {
+        this.height = height;
+        this.width = width;
+
+        compisitionOfTheWorld = new String[height][width];
+
+        System.out.println(compisitionOfTheWorld[0].length);
+        initTheWorld();
+
+        System.out.println(toString());
+    }
+
+
+    public String[][] getWorld(){
+        return compisitionOfTheWorld;
+    }
+
+    public int[] getSize(){
+        int[] size = {width, height};
+
+        return size;
+    }
+
+
+    private void changeTileOfTheWorld(int x, int y, String what){
+        this.compisitionOfTheWorld[y][x] = what;
+    }
+
+    private void initTheWorld(){
+        for(int y = 0; y < compisitionOfTheWorld.length; y ++){
+            for(int x = 0; x < compisitionOfTheWorld[y].length;x ++){
+                changeTileOfTheWorld(x, y, "");
+            }
+        }
+
+        addFloor();
+        addWalls();
+        addCeiling();
+    }
+
+    public String compositionOfTheTile(int x, int y){
+        return this.compisitionOfTheWorld[y][x];
+    }
+
+    private void addFloor(){
+        for(int y = compisitionOfTheWorld.length - 2; y < compisitionOfTheWorld.length; y ++){
+            for(int x = 0; x < compisitionOfTheWorld[y].length;x ++){
+                changeTileOfTheWorld(x, y, "floor");
+            }
+        }
+    }
+
+    private void addWalls(){
+        for(int y = 0; y < compisitionOfTheWorld.length; y ++){
+            changeTileOfTheWorld(0, y, "floor");
+        }
+
+        for(int y = 0; y < compisitionOfTheWorld.length; y ++){
+            changeTileOfTheWorld(width-1, y, "floor");
+        }
+    }
+
+    private void addCeiling(){
+        for(int x = 0; x < compisitionOfTheWorld[0].length;x ++){
+            changeTileOfTheWorld(x, 0, "floor");
+        }
+    }
+
+
+    @Override
+    public String toString(){
+        String ret = "";
+
+        for(int y = 0; y < compisitionOfTheWorld.length; y ++){
+            for(int x = 0; x < compisitionOfTheWorld[y].length;x ++){
+                switch (compositionOfTheTile(x, y)){
+                    case "":
+                        ret += "0";
+                        break;
+                    case "floor" :
+                        ret += "F";
+                       break;
+                }
+            }
+            ret += "\n";
+        }
+
+
+        return ret;
+    }
+}

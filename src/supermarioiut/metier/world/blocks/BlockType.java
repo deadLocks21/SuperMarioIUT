c92b78cd2c878954c13cc6d611cbe42adde9568d
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-05-25 10:32:14 +0200

    Create BlockType.java

---
+++ src/supermarioiut/metier/world/blocks/BlockType.java
@@ -0,0 +1,40 @@
+package supermarioiut.metier.world.blocks;
+
+public enum BlockType {
+    /**
+     * Représente un bloc du sol.
+     */
+    FLOOR,
+    /**
+     * Sol du monde souterrain.
+     */
+    BLUE_FLOOR,
+    /**
+     * Mur du monde.
+     */
+    WALL,
+    /**
+     * Mur du monde souterrain.
+     */
+    BLUE_WALL,
+    /**
+     * Mur solide.
+     */
+    SOLID_WALL,
+    /**
+     * Boite contenant un champignon.
+     */
+    LUCKY_BOX,
+    /**
+     * LuckyBox récupérée.
+     */
+    OLD_LUCKY_BOX,
+    /**
+     * Bloc caché dans le monde.
+     */
+    HIDDEN_BOX,
+    /**
+     * Bloc d'air.
+     */
+    VOID
+}

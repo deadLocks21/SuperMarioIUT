06dd3b78f425e0f2111aa69a331b26d7ba78c459
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-05-25 08:26:37 +0200

    Create TheoricWorldType.java

---
+++ src/supermarioiut/TheoricWorldType.java
@@ -0,0 +1,19 @@
+package supermarioiut;
+
+/**
+ * Enumération des differents types de monde qu'on peut générer
+ */
+public enum TheoricWorldType {
+    /**
+     * Monde vide.
+     */
+    VOID,
+    /**
+     * Monde plat.
+     */
+    FLAT,
+    /**
+     * Monde en boite.
+     */
+    BOX
+}

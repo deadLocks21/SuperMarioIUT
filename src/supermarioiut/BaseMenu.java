2ded1fcc3802044c081f3f9068b4c1a80916581d
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-05-25 09:40:29 +0200

    Implémentation de la classe BaseMenu.

---
+++ src/supermarioiut/BaseMenu.java
@@ -0,0 +1,38 @@
+package supermarioiut;
+
+import iut.Game;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+/**
+ * Menu de démarage du jeu.
+ */
+public class BaseMenu extends DisplayWorld implements KeyListener {
+    /**
+     * Constructeur de BaseMenu qui permet d'initialiser les variables.
+     *
+     * @param game         Partie à affecter.
+     * @param theoricWorld TheoricWorld à afficher.
+     */
+    public BaseMenu(Game game, TheoricWorld theoricWorld) {
+        super(game, theoricWorld);
+    }
+
+
+    // TODO Gérer les touches pour le menu.
+    @Override
+    public void keyTyped(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent keyEvent) {
+
+    }
+}

81485329e302877f227def0d13bf8028b45829b2
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-05-25 09:25:56 +0200

    Implémentation de DisplayWorld.

---
+++ src/supermarioiut/DisplayWorld.java
@@ -0,0 +1,41 @@
+package supermarioiut;
+
+import iut.Game;
+
+/**
+ * Permet d'afficher un monde.
+ */
+public class DisplayWorld {
+    /**
+     * Stocke la partie
+     */
+    Game game;
+    /**
+     * Stocke le monde à afficher.
+     */
+    TheoricWorld theoricWorld;
+
+
+    /**
+     * Constructeur qui permet d'initialiser les variables.
+     *
+     *
+     * @param game         Partie à affecter.
+     * @param theoricWorld TheoricWorld à afficher.
+     */
+    public DisplayWorld(Game game, TheoricWorld theoricWorld){
+        this.game = game;
+        this.theoricWorld = theoricWorld;
+    }
+
+
+    /**
+     * Affiche theoricWorld.
+     *
+     *
+     * @param pixelForBlock Taille d'un bloc en pixel.
+     */
+    public void display(int pixelForBlock){
+        // TODO Implémenter l'affichage du monde.
+    }
+}
